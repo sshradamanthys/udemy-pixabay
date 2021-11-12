40bd3bef095c0297e1b3d3d1d890867458cf69e3
Denis Valdivieso <sshshaka@gmail.com>
2021-11-11 22:31:33 -0400

    pagination previous & next buttons, scrollIntoView

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
   const [search, setSearch] = useState("");
   const [images, setImages] = useState([]);
   // pagination
-  // const [actual, setActual] = useState(1);
-  const [total, setTotal] = useState(5);
-  console.log(images, total);
+  const [actual, setActual] = useState(1);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     if (search === "") return;
     const fetchAPI = async () => {
       const pages = 30;
       const key = "24299186-4b265e95fd50726ef2a95bccd";
-      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${pages}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${pages}&page=${actual}`;
       const res = await fetch(url);
       const json = await res.json();
       const hits = json.hits;
@@ -26,19 +25,58 @@ function App() {
       // calculating total
       const pagesTotal = Math.ceil(totalHits / pages);
       setTotal(pagesTotal);
+
+      // scroll to top (jumbotron)
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({ behavior: "smooth" });
     };
 
     fetchAPI();
-  }, [search]);
+  }, [search, actual]);
+
+  const previousPage = () => {
+    let newActual = actual - 1;
+    if (newActual === 0) return;
+    setActual(newActual);
+  };
+
+  const nextPage = () => {
+    let newActual = actual + 1;
+    if (newActual > total) return;
+    setActual(newActual);
+  };
+
   return (
     <div className="container">
       <div className="jumbotron mt-5">
         <p className="lead text-center">Buscador de Imágenes</p>
         <Form setSearch={setSearch} />
       </div>
-      <div className="row justify-content-center">
-        <Images images={images} />
-      </div>
+      {images.length !== 0 && (
+        <div className="d-flex justify-content-center">
+          <Images images={images} />
+        </div>
+      )}
+
+      {images.length !== 0 && (
+        <div className="d-flex justify-content-around mb-5">
+          {actual === 1 ? null : (
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={previousPage}
+            >
+              &laquo; Anterior{" "}
+            </button>
+          )}
+
+          {actual === total ? null : (
+            <button type="button" className="btn btn-info" onClick={nextPage}>
+              Siguiente &raquo;
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -16,17 +16,17 @@ const Form = ({ setSearch }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="form-group col-md-9">
+      <div className="row w-50">
+        <div className="col-md-8">
           <input
             type="text"
-            className="form-control form-control-lg"
+            className="form-control"
             placeholder="Busca una imagen"
             onChange={(e) => setState(e.target.value)}
           />
         </div>
-        <div className="form-group col-md-3 mx-auto">
-          <input type="submit" className="btn btn-info btn-lg" value="Buscar" />
+        <div className="col-md-4">
+          <input type="submit" className="btn btn-info" value="Buscar" />
         </div>
       </div>
       {error ? <Error msg="Agrega un término de búsqueda" /> : null}

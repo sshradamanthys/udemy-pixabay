2b34fdb5cf6161156d6cd51c532ef2554e0e6aab
Denis Valdivieso <sshshaka@gmail.com>
2021-11-11 20:11:21 -0400

    state para el termino de busqueda

---
+++ src/components/Error.js
@@ -0,0 +1,5 @@
+const Error = ({ msg }) => (
+  <p className="my-3 p-4 text-center alert alert-primary">{msg}</p>
+);
+
+export default Error;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,35 @@
+import { useState } from "react";
+import Error from "./Error";
+
 const Form = () => {
+  const [state, setState] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.trim() === "") {
+      setError(true);
+    }
+    setError(false);
+    // setSearch(state);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="row">
         <div className="form-group col-md-9">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca una imagen"
+            onChange={(e) => setState(e.target.value)}
           />
         </div>
         <div className="form-group col-md-3 mx-auto">
-          <input
-            type="submit"
-            className="btn btn-info btn-block"
-            value="Buscar"
-          />
+          <input type="submit" className="btn btn-info btn-lg" value="Buscar" />
         </div>
       </div>
+      {error ? <Error msg="Agrega un término de búsqueda" /> : null}
     </form>
   );
 };
